5377ed339d452580d09c242c8605ccc44503a52c
siobhankb <siobhan.boylan@gmail.com>
2022-06-21 14:46:23 -0500

    added Nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Nav from './components/Nav';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+  <div id="fromApp" >
+    <Nav />
+    <h1>Hello World</h1>
+  </div>
+  );
+  
 }
 
 export default App;
